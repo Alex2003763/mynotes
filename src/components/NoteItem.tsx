49871b8c7813f4ff7dc3823df7e8940167d15b55
Alex2003763 <alex89646969@gmail.com>
2025-06-12 11:44:30 +0800

    Add files via upload

--- src/components/NoteItem.tsx
+++ src/components/NoteItem.tsx
@@ -43,11 +43,12 @@ const NoteItemComponent: React.FC<NoteItemProps> = ({ note, isSelected, onSelect
   
   // Memoize expensive computations
   const summary = useMemo(() => {
-    const contentSummary = markdownToSummaryText(note.content);
+    const fullContent = note.pages?.map(p => p.content).join('\n') || '';
+    const contentSummary = markdownToSummaryText(fullContent);
     return contentSummary.length > MAX_SUMMARY_LENGTH
       ? `${contentSummary.substring(0, MAX_SUMMARY_LENGTH)}...`
       : contentSummary;
-  }, [note.content]);
+  }, [note.pages]);
   
   const formattedDate = useMemo(() =>
     formatDistanceToNow(new Date(note.updatedAt), { addSuffix: true }),
@@ -138,7 +139,7 @@ export const NoteItem = React.memo(NoteItemComponent, (prevProps, nextProps) => 
   return (
     prevProps.note.id === nextProps.note.id &&
     prevProps.note.title === nextProps.note.title &&
-    prevProps.note.content === nextProps.note.content &&
+    JSON.stringify(prevProps.note.pages) === JSON.stringify(nextProps.note.pages) &&
     prevProps.note.updatedAt === nextProps.note.updatedAt &&
     prevProps.note.tags.length === nextProps.note.tags.length &&
     prevProps.note.tags.every((tag, index) => tag === nextProps.note.tags[index]) &&
