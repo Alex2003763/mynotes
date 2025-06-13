eb1f248016e2b30b2d37347869b348da6c5d1e6c
Alex2003763 <alex89646969@gmail.com>
2025-06-13 22:16:02 +0800

    Add files via upload

--- pwa-options.ts
+++ pwa-options.ts
@@ -22,6 +22,7 @@ export const pwaOptions: Partial<VitePWAOptions> = {
     ],
   },
   workbox: {
+    globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
     maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
   },
 };
